23f37e66220153111fdfb454a15899acb3bbfe4b
najlaaalsmadi <najlaak399@gmail.com>
2024-08-06 14:46:27 +0300

    update new 2024-8-6

--- WebApplication16/Controllers/HomeController.cs
+++ WebApplication16/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication16.Models;
 
 namespace WebApplication16.Controllers
 {
     public class HomeController : Controller
     {
+        private ElectionsEntities1 db = new ElectionsEntities1();
+
         public ActionResult Index()
         {
             return View();
@@ -18,6 +21,44 @@ namespace WebApplication16.Controllers
             ViewBag.Message = "Your application description page.";
 
             return View();
+        }
+        public ActionResult LoginAdmin()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LoginAdmin(Admin model)
+        {
+            if (ModelState.IsValid)
+            {
+                var admin = db.Admins
+                    .Where(a => a.email == model.email && a.password == model.password)
+                    .SingleOrDefault();
+
+                if (admin != null)
+                {
+                    // تسجيل الدخول ناجح، يمكنك التوجيه إلى صفحة أخرى أو إنشاء جلسة
+                    Session["Admin"] = admin;
+                    return RedirectToAction("IndexAdmin", "partyLists"); // أو أي صفحة تريد الانتقال إليها بعد تسجيل الدخول
+                }
+                else
+                {
+                    // البيانات غير صحيحة
+                    ModelState.AddModelError("", "البريد الإلكتروني أو كلمة المرور غير صحيحة");
+                }
+            }
+
+            return View(model);
+        }
+        public ActionResult Logout()
+        {
+            // قم بحذف بيانات الجلسة
+            Session.Clear();
+            Session.Abandon();
+
+            // إعادة التوجيه إلى صفحة تسجيل الدخول أو الصفحة الرئيسية
+            return RedirectToAction("LoginAdmin", "Home");
         }
         public ActionResult QA() { return View(); }
         public ActionResult ploice() { return  View(); }
